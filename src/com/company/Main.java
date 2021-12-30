43548aeba25483b0c59f64f22f88fdb95867038b
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2021-12-30 19:02:17 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Calendar cal=Calendar.getInstance();
+        System.out.println();
+        System.out.println("Year:"+cal.get(Calendar.YEAR));
+        System.out.println("Month:"+cal.get(Calendar.MONTH));
+        System.out.println("Day:"+cal.get(Calendar.DATE));
+        System.out.println("Hour:"+cal.get(Calendar.HOUR));
+        System.out.println("Minute:"+cal.get(Calendar.MINUTE));
+        System.out.println();
+	// write your code here
+    }
+}
